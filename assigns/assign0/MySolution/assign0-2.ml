7da64b30feba1e60a0c2d63a48d9d27bd1f4fee5
AznRapunzel <chenmichelle2002@gmail.com>
2023-09-13 10:38:24 -0400

    added the questions to each file

--- assigns/assign0/MySolution/assign0-2.ml
+++ assigns/assign0/MySolution/assign0-2.ml
@@ -1,5 +1,10 @@
 (* ****** ****** *)
-
+(*
+Assign0-2: 10 points
+Please implement a function that tests whether
+a given natural number is a prime:
+fun isPrime(n0: int): bool
+*)
 #use "./../assign0.ml";;
 (* ****** ****** *)
 let rec
--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,17 +1,15 @@
-(* ****** ****** *)
-
-
-
+(*
+Assign0-3: 10 points
+Please implement a function that converts a given
+integer to a string that represents the integer:
+fun int2str(i0: int): string
+*)
 (* ****** ****** *)
 let chr = Char.chr
 let ord = Char.code
 let string_init = String.init
 let len = 0
 let int2str(i0: int): string =
-  if i0 < 10 then string_init 1 (fun i0 -> chr(ord('0') + i0))
-  else string_init (i0 mod 10) (fun i0 -> chr(ord('0') + i0))
-
-(*helper function to determine the length of the int*)
-let lenOfInt(i0:int): int =
-  if i0 < 10 then 1 else i0 mod 10;;
-  
+  if i0 < 10 then string_init 1 (fun i -> chr(ord('0') + i0))
+  else string_init (i0 mod 10) (fun i -> chr(ord('0') + i0))
+;;
