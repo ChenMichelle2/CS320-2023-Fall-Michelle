2a4cb0e8fabe5e15324e2b40c94a5604a5dc41ae
AznRapunzel <chenmichelle2002@gmail.com>
2023-09-13 00:35:45 -0400

    worked on problem 3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -7,4 +7,5 @@ let chr = Char.chr
 let ord = Char.code
 let string_init = String.init
 let int2str(i0: int): string =
-  string_init i0 (fun i0 -> chr(ord('0') + i0));;
+  string_init 2 =  (fun int2str -> chr(ord('0') + i0));;
+  
