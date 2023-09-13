b2157f2426c7f58033c62b5152b78d4f6d476418
AznRapunzel <chenmichelle2002@gmail.com>
2023-09-13 09:23:43 -0400

    fixed syntax error on question 0-3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -7,5 +7,5 @@ let chr = Char.chr
 let ord = Char.code
 let string_init = String.init
 let int2str(i0: int): string =
-  string_init 2 =  (fun int2str -> chr(ord('0') + i0));;
+  string_init 2  (fun i0 -> chr(ord('a') + i0));;
   
