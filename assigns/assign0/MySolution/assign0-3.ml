a5dc970d16591afe7a9bf85065c1c33da2e5e064
AznRapunzel <chenmichelle2002@gmail.com>
2023-09-13 10:04:54 -0400

    figured out how to find the len of the int

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -6,6 +6,12 @@
 let chr = Char.chr
 let ord = Char.code
 let string_init = String.init
+let len = 0
 let int2str(i0: int): string =
-  string_init 2  (fun i0 -> chr(ord('a') + i0));;
+  if i0 < 10 then string_init 1 (fun i0 -> chr(ord('0') + i0))
+  else string_init (i0 mod 10) (fun i0 -> chr(ord('0') + i0))
+
+(*helper function to determine the length of the int*)
+let lenOfInt(i0:int): int =
+  if i0 < 10 then 1 else i0 mod 10;;
   
