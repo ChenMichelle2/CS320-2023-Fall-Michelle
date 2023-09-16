c9a9781ce436cefdafb385bd26afec114cb79066
AznRapunzel <chenmichelle2002@gmail.com>
2023-09-16 19:51:32 -0400

    did assign 1-1

---
+++ assigns/assign1/MySolution/assign1-1.ml
@@ -0,0 +1,25 @@
+
+
+(*
+assign1-1: 10 points
+Given a natural number n that is not a multiple
+of 10, intrev10(n) is the natural number whose
+representation in base 10 reverses that of the
+number n.
+
+fun intrev10(n: int): int
+
+For instance, if n = 12345, then intrev10(n) is
+54321; if n = 10203, then intrev10(n) is 30201.
+
+Please give a TAIL-RECURSIVE implementation of
+intrev10.
+*)
+
+let intrev10(n: int): int=
+  let rec singleInt(x:int)(y:int): int =
+    if x < 10 then (y*10)+x
+    else singleInt(x/10)((y*10)+(x mod 10))
+  in singleInt(n)(0) 
+;;
+let test =  intrev10(12345);;
