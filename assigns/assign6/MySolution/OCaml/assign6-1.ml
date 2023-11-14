4a928518a7ba51aad12cc3829f2b08ba36aedfd7
AznRapunzel <chenmichelle2002@gmail.com>
2023-11-13 19:30:26 -0500

    working on it

--- assigns/assign6/MySolution/OCaml/assign6-1.ml
+++ assigns/assign6/MySolution/OCaml/assign6-1.ml
@@ -1,7 +1,9 @@
 #use "MyOCaml.ml";;
+
 let sexpr_to_string (e : sexpr)  : string       = 
   string_make_fwork(fun work -> list_foreach e work)
-let sexpr_parse     (s : string) : sexpr option = ...
+
+let sexpr_parse     (s : string) : sexpr option = 
   match string_parse (parse_expr ()) s with
     | Some (e, []) -> Some e
     | _ -> None
